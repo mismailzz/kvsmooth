5bff67855aa32d9f063a930d7623335100e4dcf8
Muhammad Ismail <mismailzz75@gmail.com>
2022-09-03 02:12:59 -0400

    flower container fix

--- kvsmooth/profiles_project/settings.py
+++ kvsmooth/profiles_project/settings.py
@@ -160,10 +160,9 @@ STATIC_ROOT = str(os.path.join(PROJECT_DIR, 'static'))
 
 #CELERY_BROKER_URL = 'redis://localhost:6379'
 CELERY_BROKER_URL = 'redis://redis:6379' #for docker compose
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'django-db'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Asia/Karachi'
 
-CELERY_RESULT_BACKEND = 'django-db'
+
